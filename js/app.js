bf53d21ba096615bd36262bae1287cf9ef969997
jeff forrester <jeff.forrester@gmail.com>
2015-03-22 03:10:56 -0400

    layout complete. mobile tweaks

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(function(){
+	$(document).foundation();
+	console.log(11);
+	// $('#ERModal .slick').slick();
+
+
+	$(document).on('open.fndtn.reveal', '[data-reveal]', function () {
+		var modal = $(this);
+		// console.log(modal);
+		switch($(modal.context).prop('id')){
+			case 'ERModal':
+				$('#ERModal .slick div.slide').addClass('hide');
+			break;
+		}
+	});
+
+	var slicked = false;
+	$(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+		var modal = $(this);
+		// console.log(modal, modal.context);
+		console.log($(modal.context).prop('id'));
+		switch($(modal.context).prop('id')){
+			case 'ERModal':
+				console.log(2);
+				if(slicked){
+					// $('#ERModal .slick div.slide').addClass('hide');
+					$('#ERModal .slick').slick('unslick');
+				}
+				$('#ERModal .slick').slick();
+				$('#ERModal .slick div.slide').removeClass("hide");
+				slicked = true;
+				
+			break;
+		}
+	});
+});
