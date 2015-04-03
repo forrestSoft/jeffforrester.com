b2e2bffe87da7c29088bbf89bb6106d21a8ae318
jeff forrester <jeff.forrester@gmail.com>
2015-04-03 11:47:18 -0400

    wire 'view video button'

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 $(function(){
+	$('#ERModal .slick').slick({
+	// mobileFirst: true,
+		adaptiveHeight: true,
+		respondTo: 'window',
+		variableWidth: false,
+		responsive: [{
+			breakpoint: 600,
+			settings: {
+				arrows: true
+			}
+		},
+		{
+			breakpoint: 500,
+			settings: {
+				arrows: false
+			}
+		}]
+	});
+
+	$('#ERModal video').css({width: '98%', border: '1px solid #ccc'});
+	$('#ERModal .slick div.slide').removeClass("hide");
+
 	$(document).foundation();
-	// App.start();
-	console.log(11);
-	// $('#ERModal .slick').slick();
 
 
 	$(document).on('open.fndtn.reveal', '[data-reveal]', function () {
 		var modal = $(this);
-		// console.log(modal);
 		switch($(modal.context).prop('id')){
 			case 'ERModal':
 				$('#ERModal .slick div.slide').addClass('hide');
@@ -18,39 +36,18 @@ $(function(){
 	var slicked = false;
 	$(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
 		var modal = $(this);
-		// console.log(modal, modal.context);
 		console.log($(modal.context).prop('id'));
 		switch($(modal.context).prop('id')){
 			case 'ERModal':
-				console.log(2);
-				if(slicked){
-					$('#ERModal .slick div.slide').addClass('hide');
-					$('#ERModal .slick').slick('unslick');
-				}
-				$('#ERModal .slick').slick({
-					// mobileFirst: true,
-					adaptiveHeight: true,
-					respondTo: 'window',
-					variableWidth: false,
-					responsive: [{
-				      breakpoint: 600,
-				      settings: {
-				        arrows: true
-				      }
-				    },
-				    {
-				      breakpoint: 600,
-				      settings: {
-				        arrows: false
-				      }
-				    }]
-				});
-				$('#ERModal .slick div.slide').removeClass("hide");
-				$('#ERModal video').css({width: '98%', border: '1px solid #ccc'});
 				$('#ERModal .slick').slick("setPosition", 0);
-				slicked = true;
-				
+				$('#ERModal .slick div.slide').removeClass("hide");
+				// $(window).trigger('resize');
 			break;
 		}
+	});
+
+
+	$('.nextSlide').on('click', function(){
+		$('#ERModal .slick').slick('slickNext');
 	});
 });
