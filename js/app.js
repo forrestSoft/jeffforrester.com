b3c5898495179fcacb9e25744a39ae20e7fd4e84
jeff forrester <jeff.forrester@gmail.com>
2015-04-03 21:40:23 -0400

    tweak font loading script
    clean up html

--- js/app.js
+++ js/app.js
@@ -51,8 +51,13 @@ $(function(){
 		$('#ERModal .slick').slick('slickNext');
 	});
 
+	var unblurred = false;
 	$(document.body).on('fonts:allLoaded', function(e){
+		if(unblurred){
+			return;
+		}
 		console.log('fonts');
+		unblurred = true;
 		$('.main').addClass('uncover');
 		$('.loading').addClass('fadeOut');
 	});
@@ -60,28 +65,35 @@ $(function(){
 	WebFont.load({});
 
 	var images = [
-	'images/banners/cpg.png',
-	'images/banners/game.png',
-	'images/banners/er.png'
-];
+		'images/banners/cpg.png',
+		'images/banners/game.png',
+		'images/banners/er.png'
+	];
 
-var current;
-var total = images.length + 1;
-var img;
-for (var i = 0; i < images.length; i++) {
-	img = $("<img />").attr("src", images[i]);
-	img.on('load', function(){
-		console.log('loaded', img);
-		current++;
-	});
-}
-
-var timeoutFunction = function(id){
-	console.log(window.timeoutID);
-	if (window.fontsLoaded === true){
-		$(document.body).trigger('fonts:allLoaded');
-		clearInterval(window.timeoutID);
+	var current;
+	var total = images.length + 1;
+	var img;
+	for (var i = 0; i < images.length; i++) {
+		img = $("<img />").attr("src", images[i]);
+		img.on('load', function(){
+			console.log('loaded', img);
+			current++;
+		});
 	}
-};
-window.timeoutID = window.setInterval(timeoutFunction, 1000, this);
+
+	var fontChecks = 0;
+	var timeoutFunction = function(id){
+		// console.log(window.timeoutID);
+		if (fontChecks >=4){
+			return;
+		}
+
+		if (window.fontsLoaded === true){
+			$(document.body).trigger('fonts:allLoaded');
+			clearInterval(window.timeoutID);
+		}
+
+		fontChecks ++;
+	};
+	window.timeoutID = window.setInterval(timeoutFunction, 1000, this);
 });
